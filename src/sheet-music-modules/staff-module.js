d2dc3d1c694a909bd7dafbc91416832261427927
ntphi <phinguyen202@gmail.com>
2019-09-26 23:14:22 +0700

    draw demo Staff

---
+++ src/sheet-music-modules/staff-module.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export class ClefStaff extends React.Component {
+    render() {
+        return (
+            <Staff></Staff>
+        )
+    }
+}
+
+class Staff extends React.Component {
+    render() {
+        return (
+            <svg height="80" width="500">
+                <line x1="0" y1="20" x2="500" y2="20" stroke="black" />
+                <line x1="0" y1="30" x2="500" y2="30" stroke="black" />
+                <line x1="0" y1="40" x2="500" y2="40" stroke="black" />
+                <line x1="0" y1="50" x2="500" y2="50" stroke="black" />
+                <line x1="0" y1="60" x2="500" y2="60" stroke="black" />
+            </svg>
+        )
+    }
+}
