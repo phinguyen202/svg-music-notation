c92621d6220245fe54123d3e1278b0ad4f7587dc
phinguyen202 <phinguyen202@gmail.com>
2021-07-10 15:11:01 +0000

    convert mei to object using xml2js

--- src/components/mei.tsx
+++ src/components/mei.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Staff from '@base/staff-ledger/staff';
 import { WidthDimension } from "@model/common.model";
 import { MeiHead, MeiModel, Music } from "@model/mei";
+import { parseString } from 'xml2js';
 
 export interface MeiProp extends WidthDimension {
     source: MeiModel | Music | MeiHead;
@@ -9,6 +10,19 @@ export interface MeiProp extends WidthDimension {
 }
 
 export default function MEI({ source, config, width }: MeiProp) {
+    const [store, setStore] = useState(undefined);
+
+    useEffect(() => {
+        // validate mei by schema
+        parseString(source, function (error: any, result: any) {
+            if (error) {
+                setStore(undefined);
+                throw error;
+            } else {
+                setStore(result);
+            }
+        });
+    }, [source]);
 
     return (<>
         <Staff.JSX lineNumber={5} width={width} />
