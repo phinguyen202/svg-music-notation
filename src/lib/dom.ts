4befb6461597ad4236626ab5824bd73c54f1b9d9
ntphi <phinguyen202@gmail.com>
2021-08-08 23:45:16 +0700

    change elt dom function

--- src/lib/dom.ts
+++ src/lib/dom.ts
@@ -1,10 +1,29 @@
-export function elt(type: keyof HTMLElementTagNameMap | keyof SVGElementTagNameMap | keyof HTMLElementDeprecatedTagNameMap, 
-    props: any, ...children: (any | String)[]) {
+import { SvgError } from "@exception/root";
+import Component from "./component";
+
+type Type = keyof HTMLElementTagNameMap | keyof SVGElementTagNameMap | keyof HTMLElementDeprecatedTagNameMap;
+
+type ChildType<T> = string | HTMLElement | ComponentChildType<T>;
+
+interface ComponentChildType<T> {
+    fn: Function;
+    store?: T;
+}
+
+export function elt<T>(type: Type, props: any, ...children: Array<ChildType<T>>) {
     let dom = document.createElement(type);
     if (props) Object.assign(dom, props);
     for (let child of children) {
-        if (typeof child != "string") dom.appendChild(child);
-        else dom.appendChild(document.createTextNode(child));
+        if (child instanceof HTMLElement) {
+            dom.appendChild(child);
+        } else if (typeof child === 'string') {
+            dom.appendChild(document.createTextNode(child));
+        } else if (typeof child === 'object') {
+            const { fn, store } = child;
+            fn({ store, parent: dom });
+        } else {
+            throw new SvgError('Child param must be "string or Function or HTMLElement"');
+        }
     }
     return dom;
 }
