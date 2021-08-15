330eb958804735b60b0ac7aa626dddf3d495f68b
ntphi <phinguyen202@gmail.com>
2021-08-15 18:10:10 +0700

    Removed tsx files

--- src/components/stave/mapping/treble.key-signature.map.ts
+++
@@ -1,6 +0,0 @@
-import { AccidentalKeySignature } from '@model/business.model';
-
-export default new Map<AccidentalKeySignature, number[]>([
-    ['flat', [25, 10, 30, 15, 35, 20, 40]],
-    ['sharp', [5, 20, 0, 15, 30, 10, 25]],
-]);
--- src/components/stave/stave.model.ts
+++
@@ -1,8 +0,0 @@
-import { YCoordinate, Order } from '@model/common.model';
-
-export interface NoteConfig extends YCoordinate, Order<number> {
-    isStemUp: boolean,
-    ledgers?: Array<number>
-}
-
-export interface RestConfig extends YCoordinate { }
